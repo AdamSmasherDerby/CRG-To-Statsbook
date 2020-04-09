3b97018a2a1cbed4599359809cc1c7dbcabb6f67
Peter Elliott <pelliott@official-sounding.com>
2020-04-09 15:03:46 -0500

    halfway done on penalties

--- src/writers/xlsxWriter.js
+++ src/writers/xlsxWriter.js
@@ -1,6 +1,7 @@
 const XLP = require('xlsx-populate')
 const sbTemplate = require('../assets/2018statsbook.json')
 const teamNames = ['home', 'away']
+const periods = [1,2]
 
 class XlsxWriter {
     constructor(workbook, newBook) {
@@ -48,7 +49,7 @@ class XlsxWriter {
             const numberCell = rowcol(sbTemplate.teams[team].firstNumber)
             const nameCell = rowcol(sbTemplate.teams[team].firstName)
             const maxNum = sbTemplate.teams[team].maxNum
-            const teamSkaters = Array.from(skaters[team])
+            const teamSkaters = Object.values(skaters[team])
 
             teamSkaters.forEach((skater) => {
                 const name = skater.name
@@ -60,12 +61,120 @@ class XlsxWriter {
             })
 
 
+            // remove remaining values (if any) in the skater section of the IGRF
             for (let s = teamSkaters.length; s < maxNum; s++) {
                 let row = numberCell.r
                 workbook.sheet(teamSheet).row(row + s).cell(numberCell.c).value('')
                 workbook.sheet(teamSheet).row(row + s).cell(nameCell.c).value('')
             }
         })
+    }
+
+    penalties(skaters) {
+        // Update the penalty data in the statsbook from the CRG data
+        const sheet = this.workbook.sheet(sbTemplate.penalties.sheetName)
+        const expRe = /EXP-(\w)/
+
+        teamNames.forEach((teamName, teamIdx) => {
+            const team = skaters[teamName]
+            const crgTeam = this.crgData[teamIdx]
+            
+            periods.forEach((period) => {
+                const template = sbTemplate.penalties[period][teamName]
+
+                const firstPenaltyCell = rowcol(template.firstPenalty)
+                const firstJamCell = rowcol(template.firstJam)
+                
+                const firstFOCell = rowcol(template.firstFO)
+                const firstFOJamCell = rowcol(template.firstFOJam)
+
+                Object.keys(team).forEach((skaterId) => {
+                    const skater = team[skaterId];
+                    const crgSkater = crgTeam.skaters.find(s => s.id === skaterId)
+
+                    const penaltyRow = firstPenaltyCell.r + (skater.row * 2)
+                    const jamRow = firstJamCell.r + (skater.row * 2)
+
+                    let lastPenaltyCode = 'EXP'
+
+                    if(skaterData && skaterData.penalties.length) {
+                        const penalties = skaterData.penalties
+                        lastPenaltyCode = penalties[penalties.length - 1].code
+                        
+                    }
+                })
+            })
+        })
+        for (let t in teamNames) {
+            // For each team
+            let teamName = teamNames[t]
+            let team = skaters[teamName]
+
+            for (let p = 1; p < 3; p++) {
+                // For each period
+                let firstPenaltyCell = rowcol(sbTemplate.penalties[p][teamName].firstPenalty)
+                let pFirstCol = firstPenaltyCell.c
+
+                let firstJamCell = rowcol(sbTemplate.penalties[p][teamName].firstJam)
+                let jFirstCol = firstJamCell.c
+
+                let firstFOCell = rowcol(sbTemplate.penalties[p][teamName].firstFO)
+                let firstFOJamCell = rowcol(sbTemplate.penalties[p][teamName].firstFOJam)
+
+                for (let skaterID in team) {
+                    // For each skater on the team
+
+                    let skater = team[skaterID]
+                    let skaterData = crgData.teams[t].skaters.find(x => x.id == skaterID)
+                    let penaltyRow = firstPenaltyCell.r + (skater.row * 2)
+                    let jamRow = firstJamCell.r + (skater.row * 2)
+                    let lastPenaltyCode = 'EXP'
+
+                    if (skaterData != undefined && skaterData.penalties.length > 0) {
+                        // If they have any penalties, add them
+
+                        let plist = skaterData.penalties
+                        lastPenaltyCode = plist[plist.length - 1].code
+
+                        let priorPenalties = plist.filter(x => x.period < p).length
+                        let penaltyCol = pFirstCol + priorPenalties
+                        let jamCol = jFirstCol + priorPenalties
+                        plist = plist.filter(x => x.period == p)
+
+                        for (let pen in plist) {
+                            let code = plist[pen].code
+                            let jam = plist[pen].jam
+
+                            workbook.sheet(sheet).row(penaltyRow).cell(penaltyCol).value(code)
+                            workbook.sheet(sheet).row(jamRow).cell(jamCol).value(jam)
+
+                            penaltyCol += 1
+                            jamCol += 1
+                        }
+
+                    }
+
+                    if (skaterData != undefined
+                        && skaterData.hasOwnProperty('fo_exp')
+                        && skaterData.fo_exp.period == p
+                    ) {
+                        let code = '??'
+                        if (skaterData.fo_exp.code == 'FO') {
+                            code = 'FO'
+                        } else if (expRe.exec(skaterData.fo_exp.code) != null) {
+                            code = expRe.exec(skaterData.fo_exp.code)[1]
+                        } else if (skaterData.fo_exp.code == 'EXP') {
+                            code = lastPenaltyCode
+                        } else if (/^[ABCDEFGHILMNPX]{1}/.test(skaterData.fo_exp.code)) {
+                            code = skaterData.fo_exp.code
+                        }
+                        let jam = skaterData.fo_exp.jam
+                        workbook.sheet(sheet).row(penaltyRow).cell(firstFOCell.c).value(code)
+                        workbook.sheet(sheet).row(jamRow).cell(firstFOJamCell.c).value(jam)
+                    }
+                }
+            }
+        }
     }
 }
 
